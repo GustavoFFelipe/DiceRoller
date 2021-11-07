68594ae9b066d6337404e4a80577f7fe5804c48a
GustavoFFelipe <gustavo.ffelipe19@gmail.com>
2021-11-07 16:41:12 -0300

    chance from bussniess rules

--- app/src/main/java/app/alpha/diceroller/MainActivity.kt
+++ app/src/main/java/app/alpha/diceroller/MainActivity.kt
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,16 +18,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun rollDice() { // function with de rules of the dice rolling i random
+    private fun rollDice() { // function with de rules of the dice rolling random
 
         val randomInt = java.util.Random().nextInt(6) +1
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (randomInt) {  // we defined images for which random number from dice
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else 6 -> R.drawable.dice_6
+            else  -> R.drawable.dice_6
         }
         val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
